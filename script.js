05d40fcc710083b89ea501d3679ef8df8a9cd4be
MishchenkoArtem <The.Nesh@yandex.ru>
2021-09-07 08:25:28 +0400

    popup__add-close

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const profileEditButton = document.querySelector('.profile__edit-button');
+const profileAddButton = document.querySelector('.profile__add-button');
 const popupOpened = document.querySelector('.popup_opened');
+const popupAddOpened = document.querySelector('.popup__add-opened'); 
 const popupClose = document.querySelector('.popup__close');
+const popupAddClose = document.querySelector('.popup__add-close');
 
 profileEditButton.addEventListener('click', function (){
     popupOpened.classList.remove('popup_opened');
@@ -8,4 +11,12 @@ profileEditButton.addEventListener('click', function (){
 
 popupClose.addEventListener('click', function () {
     popupOpened.classList.add('popup_opened');
+})
+
+profileAddButton.addEventListener('click', function () {
+    popupAddOpened.classList.remove('popup__add-opened');
+})
+
+popupAddClose.addEventListener('click', function () {
+    popupAddOpened.classList.add('popup__add-opened');
 })
