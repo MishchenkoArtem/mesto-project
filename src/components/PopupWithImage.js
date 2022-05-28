2a45b0625f81fa4ab69b2a5944f0983d16dcd9cf
MishchenkoArtem <The.Nesh@yandex.ru>
2022-05-28 12:17:29 +0300

    Update PopupWithImage.js

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -4,13 +4,13 @@ export default class PopupWithImage extends Popup {
     constructor(popupSelector) {
         super(popupSelector);
         this.imageSettings = this._popupSelector.querySelector('.popup__image');
-        this.imageCaption =  this._popupSelector.querySelector('.popup__title-image');
+        this.imageCaption = this._popupSelector.querySelector('.popup__title-image');
     }
 
-    open(evt){
+    open(evt) {
         super.open();
         this.imageSettings.src = evt.target.src;
-        this.imageSettings.all = evt.target.alt;
-        this.imageCaption.textContent = evt.target.alt;
+        this.imageSettings.alt = evt.target.alt;
+        this.imageCaption.textContent = evt.target.closest('.card__background').querySelector('.card__heading').textContent;
     }
 }
