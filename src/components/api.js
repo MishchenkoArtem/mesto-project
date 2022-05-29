0fa2fb29b97f7b0051bbb8f3da923e2e5cce58d4
MishchenkoArtem <The.Nesh@yandex.ru>
2022-05-29 22:10:48 +0300

    Update api.js

--- src/components/api.js
+++ src/components/api.js
@@ -54,7 +54,6 @@ export default class Api {
 
   // Добаление лайка карточки
   sendLike(likes) {
-    console.log(`${this.baseUrl}/cards/likes/${likes}`)
     return fetch(`${this.baseUrl}/cards/likes/${likes}`, {
       method: "PUT",
       headers: this.headers,
@@ -86,85 +85,3 @@ export default class Api {
     }).then((res) => this.getResponseData(res));
   }
 }
-
-// const config = {
-//   baseUrl: "https://nomoreparties.co/v1/plus-cohort-9",
-//   headers: {
-//     Authorization: "0ce4a23a-1bec-4586-80d1-c87f648e62fc",
-//     "Content-Type": "application/json",
-//   },
-// };
-
-// function getResponseData(res) {
-//   return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`);
-// }
-
-// function getUser() {
-//   return fetch(`${config.baseUrl}/users/me`, {
-//     headers: config.headers,
-//   }).then((res) => getResponseData(res));
-// }
-
-// function getCards() {
-//   return fetch(`${config.baseUrl}/cards`, {
-//     headers: config.headers,
-//   }).then((res) => getResponseData(res));
-// }
-
-// export function getAppInfo() {
-//   return Promise.all([getUser(), getCards()]);
-// }
-
-// export function profileUpdate(profileName, profileInfo) {
-//   return fetch(`${config.baseUrl}/users/me`, {
-//     method: "PATCH",
-//     headers: config.headers,
-//     body: JSON.stringify({
-//       name: profileName,
-//       about: profileInfo,
-//     }),
-//   }).then((res) => getResponseData(res));
-// }
-
-// export function newPostCard(cardName, cardImage, ownerId) {
-//   return fetch(`${config.baseUrl}/cards`, {
-//     method: "POST",
-//     headers: config.headers,
-//     body: JSON.stringify({
-//       name: cardName,
-//       link: cardImage,
-//       owner: ownerId,
-//     }),
-//   }).then((res) => getResponseData(res));
-// }
-
-// export function sendLike(like) {
-//   return fetch(`${config.baseUrl}/cards/likes/${like}`, {
-//     method: "PUT",
-//     headers: config.headers,
-//   }).then((res) => getResponseData(res));
-// }
-
-// export function removeLike(like) {
-//   return fetch(`${config.baseUrl}/cards/likes/${like}`, {
-//     method: "DELETE",
-//     headers: config.headers,
-//   }).then((res) => getResponseData(res));
-// }
-
-// export function deleteCard(cardId) {
-//   return fetch(`${config.baseUrl}/cards/${cardId}`, {
-//     method: "DELETE",
-//     headers: config.headers,
-//   }).then((res) => getResponseData(res));
-// }
-
-// export function avatarUpdate(avatar) {
-//   return fetch(`${config.baseUrl}/users/me/avatar`, {
-//     method: "PATCH",
-//     headers: config.headers,
-//     body: JSON.stringify({
-//       avatar: avatar,
-//     }),
-//   }).then((res) => getResponseData(res));
-// }
