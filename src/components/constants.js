5b6fc8472555752570e3dfb630068dbb2a2e5c92
MishchenkoArtem <The.Nesh@yandex.ru>
2022-06-05 18:49:50 +0300

    Update constants.js

--- src/components/constants.js
+++ src/components/constants.js
@@ -14,13 +14,14 @@ export const cardTemplate = document.querySelector('.template__card').content
 export const cardListSection = '.card__list'
 export const popupAvatar = document.querySelector('.popup__avatar')
 export const btnSaveAvatar = popupAvatar.querySelector('.popup__button')
-export const formProfileElement = document.forms.formProfile
+
+export const formProfileSelector = document.forms.formProfile
 export const nameInput = document.forms.formProfile.nameProfile
 export const jobInput = document.forms.formProfile.professionProfile
-export const formCardElement = document.forms.formCard
+export const formCardSelector = document.forms.formCard
 export const cardName = document.forms.formCard.cardName
 export const cardImage = document.forms.formCard.cardImage
-export const formAvatarElement = document.forms.formAvatar
+export const formAvatarSelector = document.forms.formAvatar
 export const avatarInput = document.forms.formAvatar.avatar
 
 export const formsElementsSelectors = {
